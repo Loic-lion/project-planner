cc405599ac7a7711081d2a9972baef94eb0b7f9b
Loic Lion <loic.lion.contact@gmail.com>
2023-05-04 12:00:30 +0200

    ajout du js pour créer de nouvelles taches + les saves dans le llocal storage

---
+++ assets/modules/add-tasks.js
@@ -0,0 +1,44 @@
+///////AJOUT DES TACHES + SAVE DANS LE LOCAL STORAGE//////
+export function nouvelleTache() {
+  const inputName = document.getElementById("searchbar_name");
+  const inputTasks = document.getElementById("searchbar_task");
+  const inputDate = document.getElementById("searchbar_date");
+  const list = document.getElementById("container_list");
+
+  const inputTaskDone = inputName.value;
+  const inputNameDone = inputTasks.value;
+  const inputDateDone = inputDate.value;
+
+  if (inputTaskDone.trim() == "") {
+    alert("Entrez du texte pour créer une nouvelle tâche");
+    return false;
+  }
+
+  if (inputNameDone.trim() == "") {
+    alert("Entrez du texte pour nommer une nouvelle tâche");
+    return false;
+  }
+
+  if (inputDateDone.trim() == "") {
+    alert("Entrez une date pour créer une nouvelle tâche");
+    return false;
+  }
+
+  const newTask = document.createElement("li");
+  newTask.innerHTML = ` <span>${inputNameDone}</span> 
+    <span>${inputTaskDone}</span> 
+    <span>${inputDateDone}</span> 
+    <div class="container_list_checkbox"> 
+    <input type="radio" name="${inputNameDone} value="done" />Done
+    <input type="radio" name="${inputNameDone} value="doing" />Doing
+    <input type="radio" name="${inputNameDone} value="todo" checked/>To do
+    </div> `;
+
+  list.appendChild(newTask);
+
+  const tasks = list.innerHTML;
+  localStorage.setItem("tasks", tasks);
+  inputName.value = "";
+  inputDate.value = "";
+  inputTasks.value = "";
+}
---
+++ assets/modules/script.js
@@ -0,0 +1,21 @@
+import { nouvelleTache } from "./add-tasks.js";
+
+const boutonAd = document.getElementById("input_add");
+const form = document.querySelector("form");
+
+
+//////////////////Création des tâches////////////
+boutonAd.addEventListener("click", nouvelleTache);
+////////////////////Empecher le reset du submit///////////
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+//////////////////////Local Storage////////////////////
+window.addEventListener("load", () => {
+  const list = document.getElementById("container_list");
+  const tasks = localStorage.getItem("tasks");
+  if (tasks) {
+    list.innerHTML = tasks;
+  }
+});
+// localStorage.clear();
